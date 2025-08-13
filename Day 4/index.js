cf77f86195bbc2e965a6f202b9fc7ca6aa67d0f5
Priyanshu Nayak <priyanshu.nayak.555@gmail.com>
2025-08-13 15:46:12 +0530

    ignore node module

--- Day 4/index.js
+++ Day 4/index.js
@@ -1,15 +1,24 @@
 import express from "express";
+import path from "path";
 // const express =require("express");
 const app=express(); // instance of your application
 app.get('/',(req,res)=>{
     res.send("welcome to root route")
 })
-app.get('/home',(req,res)=>{
-    res.send("welcome to root route home page")
-})
 
+app.set('view engine','ejs'); // setting view engine to ejs
+app.set('views',path.join(import.meta.dirname,'views')); // setting views folder to ./views
+// app.get('/home',(req,res)=>{
+//     res.send("welcome to root route home page")
+// })
+app.get('/priyanshu',(req,res)=>{
+    res.render('index'); // rendering ejs file
+})
+// app.get('*',(req,res)=>{
+//     res.send("404 page not found")
+// })
 const PORT=8080;
-app.listen(PORT,()=>{
+app.listen(PORT,()=>{   
     console.log(`server connected at port 1 : ${PORT}`);
     
 })
